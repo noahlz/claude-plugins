505aebeaabfa3997ac3361a04eb06b7824012fb3
Noah Zucker <nzucker@gmail.com>
2025-12-16 16:45:02 -0500

    Make reinstall script actually work.

--- reinstall.sh
+++ reinstall.sh
@@ -3,6 +3,9 @@
 echo "Reloading..."
 
 claude plugin uninstall dev-workflow@noahlz.github.io
+claude plugin marketplace remove noahlz.github.io
+
+claude plugin marketplace add ./
 claude plugin install dev-workflow@noahlz.github.io
 
 echo "Done, restart Claude Code."
